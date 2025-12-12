bf2e47b571b3a0a5fbb2d67608ec94baae33dfdb
matteodiaz <144482392+matteodiaz@users.noreply.github.com>
2025-12-12 13:32:45 -0500

    Create utils.py

---
+++ sfm/utils.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def in2hom(X):
+    return np.concatenate([X, np.ones((X.shape[0], 1), dtype=np.float32)], axis=1)
+
+def hom2in(Xh):
+    return Xh[:, :2] / Xh[:, 2:]
+
+def normalizing_transform(X):
+    D = X.shape[1]
+    centroid = X.mean(axis=0, keepdims=True)
+    denom = np.mean(np.sqrt(np.sum((X - centroid)**2, axis=1)))
+    s = np.sqrt(D) / denom
+    t = -s * centroid[0, :]
+    t = np.concatenate((t, np.array([1])))
+    return np.concatenate((s*np.eye(D+1, D+1)[:, 0:D], np.expand_dims(t, 1)), axis=1)
